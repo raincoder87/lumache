673da587a36b12264d1e4e52f2ac5d302aace7e5
hefnymah <hefnymah@gmail.com>
2022-03-18 15:39:59 +0100

    add launch bottoms

--- docs/conf.py
+++ docs/conf.py
@@ -25,6 +25,10 @@ copyright = '2022, Geothermal Energy and Geofluids, ETH Zurich'
 #copyright = '[Geothermal Energy and Geofluids, ETH Zurich](https://geg.ethz.ch)'
 author = 'GEG- ETH Zurich'
 
+sphinx_gallery_conf = {
+     'examples_dirs': '../examples',   # path to your example scripts
+     'gallery_dirs': 'auto_examples',  # path to where to save gallery generated output
+}
 
 
 # The full version, including alpha/beta/rc tags
@@ -45,7 +49,9 @@ extensions = [
     'myst_parser',
     'sphinx.ext.duration',
     'sphinx.ext.autosectionlabel',
+#    'sphinx_gallery.gen_gallery',
 #    'nbsphinx',
+    'sphinx_thebe'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -66,6 +72,26 @@ html_theme = 'sphinx_book_theme'
 
 html_logo = '../small_logo.jpg'
 html_title = 'Documentation'
+
+html_theme_options = {
+    "path_to_docs": "docs",
+    "repository_url": "https://github.com/GEG-ETHZ/genGEO",
+    "launch_buttons": {
+        "binderhub_url": "https://mybinder.org",
+        "colab_url": "https://colab.research.google.com/",
+        "deepnote_url": "https://deepnote.com/",
+        "notebook_interface": "jupyterlab",
+        "thebe": True,
+        # "jupyterhub_url": "https://datahub.berkeley.edu",  # For testing
+    },
+    "use_edit_page_button": True,
+    "use_issues_button": True,
+    "use_repository_button": True,
+    "use_download_button": True,
+    "logo_only": True,
+    "show_toc_level": 2,
+
+}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
